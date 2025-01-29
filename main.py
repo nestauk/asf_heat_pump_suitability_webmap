32138f9bfe63dff0ad9ffd44afd298a94e140380
Daniel Lewis <daniel.lewis@nesta.org.uk>
2025-01-29 16:18:37 +0000

    prototype popup

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import Field, Session, SQLModel, create_engine
+from typing import Annotated
 
 # Code here omitted 👈
 
-class AreaData(SQLModel, table=True):
+class Area_Data(SQLModel, table=True):
     area_code: str = Field(primary_key=True)
     area_name: str
     area_name_wales: str | None = None
@@ -31,13 +34,21 @@ SessionDep = Annotated[Session, Depends(get_session)]
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
 
 @app.get("/areas/{area_code}")
-def read_area(area_code: int, session: SessionDep) -> AreaData:
-    area = session.get(AreaData, area_code)
-    if not hero:
+def read_area(area_code: str, session: SessionDep) -> Area_Data:
+    area = session.get(Area_Data, area_code)
+    if not area_code:
         raise HTTPException(status_code=404, detail="area_code not recognised.")
     return area
